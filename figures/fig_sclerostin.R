2c2ef6feeec7746eaa3c91e80b3dc4c52faf8b4a
Lucas <lucasdsveras@gmail.com>
2021-07-19 14:01:37 +0100

    Add plot of sclerostin x body mass

--- figures/fig_sclerostin.R
+++ figures/fig_sclerostin.R
@@ -4,6 +4,7 @@ library(here)
 library(tidyverse)
 library(gamlj)
 library(lmerTest)
+library(patchwork)
 library(ragg)
 
 # Read data ---------------------------------------------------------------
@@ -59,9 +60,57 @@ sclerostin_loading_plot <- sclerostin_loading_plot +
     parse = TRUE
   )
 
+# Sclerostin by body mass plot --------------------------------------------
+
+sclerostin_body_mass_plot <- gamljMixed(
+  formula = sclerostin ~ 1 + whole_body_total_mass + (1 | subj) + (1 | time),
+  data = df,
+  showContrastCode = TRUE,
+  plotHAxis = whole_body_total_mass,
+  plotRaw = TRUE,
+  plotError = "ci",
+  eDesc = TRUE,
+  simpleVariable = whole_body_total_mass,
+  scaling = list(
+    list(
+      var = "whole_body_total_mass",
+      type = "none")),
+  lrtRandomEffects = TRUE,
+  plotRandomEffects = TRUE) %>%
+  gamlj_ggplot()
+
+sclerostin_body_mass_plot <- sclerostin_body_mass_plot +
+  theme(
+    axis.title.y = element_text(size = 14, face = "bold"),
+    axis.text.y = element_text(size = 12, face = "bold"),
+    axis.text.x = element_text(size = 12, face = "bold")
+  ) +
+  labs(
+    x = quote("Weight" ~ (kg)),
+    y = quote("Sclerostin" ~ ( ng %.% mL^-1))
+  ) +
+  annotate(
+    "text", x = 50, y = 1.2,
+    label = "Sclerostin = 0.653 - 3.764e-4 * weight",
+    hjust = 0
+  ) +
+  annotate(
+    "text", x = 50, y = 1.16,
+    label = "paste(italic(p) == .828, \"; \", italic(R)^2 == \"0.00\")",
+    hjust = 0,
+    parse = TRUE
+  )
+
+# Combine and save plots --------------------------------------------------
+
+plot_grid <- sclerostin_loading_plot +
+  sclerostin_body_mass_plot +
+  plot_annotation(tag_levels = "A") &
+  theme(plot.tag = element_text(size = 18, face = "bold"))
+
 agg_tiff(
   here("figures/fig_sclerostin.tiff"),
-  width = 30, height = 20, units = "cm", res = 300
+  width = 60, height = 20, units = "cm", res = 300
 )
-plot(sclerostin_loading_plot)
+plot(plot_grid)
 dev.off()
